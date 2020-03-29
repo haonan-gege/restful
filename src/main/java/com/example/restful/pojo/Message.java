b6d0e528a1f69ab0115a540d5972ba7ba2416128
Haonan1113 <1455527286@qq.com>
2020-03-29 19:32:20 +0800

    Initial commit

---
+++ src/main/java/com/example/restful/pojo/Message.java
@@ -0,0 +1,16 @@
+package com.example.restfuldemo.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private Long id;
+    private String text;
+    private String summary;
+}
